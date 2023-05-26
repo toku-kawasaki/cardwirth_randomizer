1aff1934a1d885f4834d12d9c219051c419bb824
toku-kawasaki <rinkodatoukyu@gmail.com>
2023-05-26 17:47:22 +0900

    シナリオ名を入力しなくてもいいように アドバイスメモを追加

--- hardrandomizer.py
+++ hardrandomizer.py
@@ -3,29 +3,33 @@ import os
 import shutil
 import random as rand
 
-scenario_name = input("シナリオの名前を入力してください：")
+scenario_lis = glob("./*/Summary.wsm")
 
-Mate_lis = glob(f"./{scenario_name}/Mate*.wid")
-Item_lis = glob(f"./{scenario_name}/Item*.wid")
-Info_lis = glob(f"./{scenario_name}/Info*.wid")
-Skill_lis = glob(f"./{scenario_name}/Skill.wid")
+for scenario in scenario_lis:
+    
+    scenario_name = os.path.basename(os.path.dirname(scenario))
 
-for Mate in Mate_lis:
-    Mate_file = os.path.basename(Mate)
-    new_Mate_lis = glob(f"../*/*/{Mate_file}") + glob(f"../*/*/*/{Mate_file}")
-    shutil.copy(rand.choice(new_Mate_lis),f"./{scenario_name}")
+    Mate_lis = glob(f"./{scenario_name}/Mate*.wid")
+    Item_lis = glob(f"./{scenario_name}/Item*.wid")
+    Info_lis = glob(f"./{scenario_name}/Info*.wid")
+    Skill_lis = glob(f"./{scenario_name}/Skill.wid")
 
-for Item in Item_lis:
-    Item_file = os.path.basename(Item)
-    new_Item_lis = glob(f"../*/*/{Item_file}") + glob(f"../*/*/*/{Mate_file}")
-    shutil.copy(rand.choice(new_Item_lis),f"./{scenario_name}")
+    for Mate in Mate_lis:
+        Mate_file = os.path.basename(Mate)
+        new_Mate_lis = glob(f"../*/*/{Mate_file}") + glob(f"../*/*/*/{Mate_file}")
+        shutil.copy(rand.choice(new_Mate_lis),f"./{scenario_name}")
 
-for Info in Info_lis:
-    Info_file = os.path.basename(Info)
-    new_Info_lis = glob(f"../*/*/{Info_file}") + glob(f"../*/*/*/{Mate_file}")
-    shutil.copy(rand.choice(new_Info_lis),f"./{scenario_name}")
+    for Item in Item_lis:
+        Item_file = os.path.basename(Item)
+        new_Item_lis = glob(f"../*/*/{Item_file}") + glob(f"../*/*/*/{Mate_file}")
+        shutil.copy(rand.choice(new_Item_lis),f"./{scenario_name}")
 
-for Skill in Skill_lis:
-    Skill_file = os.path.basename(Skill)
-    new_Skill_lis = glob(f"../ask/*/{Skill_file}") + glob(f"../*/*/*/{Skill_file}")
-    shutil.copy(rand.choice(new_Skill_lis),f"./{scenario_name}")
+    for Info in Info_lis:
+        Info_file = os.path.basename(Info)
+        new_Info_lis = glob(f"../*/*/{Info_file}") + glob(f"../*/*/*/{Mate_file}")
+        shutil.copy(rand.choice(new_Info_lis),f"./{scenario_name}")
+
+    for Skill in Skill_lis:
+        Skill_file = os.path.basename(Skill)
+        new_Skill_lis = glob(f"../*/*/{Skill_file}") + glob(f"../*/*/*/{Skill_file}")
+        shutil.copy(rand.choice(new_Skill_lis),f"./{scenario_name}")
--- randomizer.py
+++ randomizer.py
@@ -3,29 +3,33 @@ import os
 import shutil
 import random as rand
 
-scenario_name = input("シナリオの名前を入力してください：")
+scenario_lis = glob("./*/Summary.wsm")
 
-Mate_lis = glob(f"./{scenario_name}/Mate*.wid")
-Item_lis = glob(f"./{scenario_name}/Item*.wid")
-Info_lis = glob(f"./{scenario_name}/Info*.wid")
-Skill_lis = glob(f"./{scenario_name}/Skill*.wid")
+for scenario in scenario_lis:
 
-for Mate in Mate_lis:
-    Mate_file = os.path.basename(Mate)
-    new_Mate_lis = glob(f"../ask/*/{Mate_file}") + glob(f"../サンプルダンジョン/モンスター図鑑*/{Mate_file}")
-    shutil.copy(rand.choice(new_Mate_lis),f"./{scenario_name}")
+    scenario_name = os.path.basename(os.path.dirname(scenario))
 
-for Item in Item_lis:
-    Item_file = os.path.basename(Item)
-    new_Item_lis = glob(f"../ask/*/{Item_file}") + glob(f"../サンプルダンジョン/モンスター図鑑*/{Item_file}")
-    shutil.copy(rand.choice(new_Item_lis),f"./{scenario_name}")
+    Mate_lis = glob(f"./{scenario_name}/Mate*.wid")
+    Item_lis = glob(f"./{scenario_name}/Item*.wid")
+    Info_lis = glob(f"./{scenario_name}/Info*.wid")
+    Skill_lis = glob(f"./{scenario_name}/Skill*.wid")
 
-for Info in Info_lis:
-    Info_file = os.path.basename(Info)
-    new_Info_lis = glob(f"../ask/*/{Info_file}") + glob(f"../サンプルダンジョン/モンスター図鑑*/{Info_file}")
-    shutil.copy(rand.choice(new_Info_lis),f"./{scenario_name}")
+    for Mate in Mate_lis:
+        Mate_file = os.path.basename(Mate)
+        new_Mate_lis = glob(f"../ask/*/{Mate_file}") + glob(f"../サンプルダンジョン/モンスター図鑑+/{Mate_file}")
+        shutil.copy(rand.choice(new_Mate_lis),f"./{scenario_name}")
 
-for Skill in Skill_lis:
-    Skill_file = os.path.basename(Skill)
-    new_Skill_lis = glob(f"../ask/*/{Skill_file}") + glob(f"../サンプルダンジョン/モンスター図鑑*/{Skill_file}")
-    shutil.copy(rand.choice(new_Skill_lis),f"./{scenario_name}")
+    for Item in Item_lis:
+        Item_file = os.path.basename(Item)
+        new_Item_lis = glob(f"../ask/*/{Item_file}") + glob(f"../サンプルダンジョン/モンスター図鑑+/{Item_file}")
+        shutil.copy(rand.choice(new_Item_lis),f"./{scenario_name}")
+
+    for Info in Info_lis:
+        Info_file = os.path.basename(Info)
+        new_Info_lis = glob(f"../ask/*/{Info_file}") + glob(f"../サンプルダンジョン/モンスター図鑑+/{Info_file}")
+        shutil.copy(rand.choice(new_Info_lis),f"./{scenario_name}")
+
+    for Skill in Skill_lis:
+        Skill_file = os.path.basename(Skill)
+        new_Skill_lis = glob(f"../ask/*/{Skill_file}") + glob(f"../サンプルダンジョン/モンスター図鑑+/{Skill_file}")
+        shutil.copy(rand.choice(new_Skill_lis),f"./{scenario_name}")
